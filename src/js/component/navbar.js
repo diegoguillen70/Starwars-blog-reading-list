20b72450955bfd4e3547b0bdf715bac4477bef2f
diegoguillen70 <diegoguillen70@gmail.com>
2023-09-03 20:24:41 +0000

    changed branch name

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,15 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import start_war from "../../img/start_war.png";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
+		<nav className="navbar navbar-light bg-light d-flex justify-content-betewen">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<span className="navbar-brand mx-5 h1" ><img src={start_war} style={{width: "80px"}}/></span>
 			</Link>
 			<div className="ml-auto">
 				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
+					<button className="btn btn-primary me-5">Favorities</button>
 				</Link>
 			</div>
 		</nav>
