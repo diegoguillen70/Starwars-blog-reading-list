3253b0c1f5ccf004eca528ffc4eafda6c039567c
diegoguillen70 <diegoguillen70@gmail.com>
2023-09-05 14:29:58 +0000

    updated navbar

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -32,6 +32,7 @@ export const Navbar = () => {
                   </li>
                 );
               })}
+
               {/* <button className="dropdown-item" type="button">
                   Action
                 </button> */}
