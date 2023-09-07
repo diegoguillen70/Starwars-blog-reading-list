6e9dd5b6d8539953cf108e43bf301b4bfab58f18
diegoguillen70 <diegoguillen70@gmail.com>
2023-09-07 03:43:59 +0000

    favorites

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,10 +1,22 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import start_war from "../../img/start_war.png";
 import { Context } from "../store/appContext";
 
 export const Navbar = () => {
   const { store, actions } = useContext(Context);
+  const [isShow, setIsShow] = useState(false);
+  const [displayDropdown, setDisplayDropdown] = useState("");
+
+  useEffect(() => {
+    window.addEventListener("show.bs.dropdown", (event) => {});
+  }, []);
+  function handleClick() {
+    setIsShow(!isShow);
+    isShow && store.favorites.length > 0
+      ? setDisplayDropdown("dropdown")
+      : setDisplayDropdown("");
+  }
   return (
     <nav className="navbar navbar-light bg-light fixed-top">
       <Link to="/">
@@ -18,16 +30,22 @@ export const Navbar = () => {
             <button
               type="button"
               className="btn btn-primary dropdown-toggle me-5"
-              data-bs-toggle="dropdown"
+              data-bs-toggle={""}
               aria-expanded="false"
+              onClick={handleClick}
             >
-              Favorities
+              Favorities {store.favorites.length}
             </button>
-            <ul className="dropdown-menu dropdown-menu-end">
+            <ul
+              className={`dropdown-menu dropdown-menu-end ${
+                isShow && store.favorites.length > 0 ? "show" : ""
+              }`}
+              data-bs-popper={isShow ? "none" : ""}
+            >
               {store.favorites.map((favorite, item) => {
                 return (
                   <li key={item} className="d-flex justify-content-between">
-                    {favorite}{" "}
+                    {favorite}
                     <p onClick={() => actions.removeFavorite(favorite)}>🗑</p>
                   </li>
                 );
